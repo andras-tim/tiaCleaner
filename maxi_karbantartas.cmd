df7cf41ed0dd75efe1b391f54f0d08eebfebb241
Andras Tim <andras.tim@gmail.com>
2009-01-12 00:24:53 +0100

    Paraméterek kezelése set_params.cmd-ben

--- maxi_karbantartas.cmd
+++ maxi_karbantartas.cmd
@@ -1,5 +1,5 @@
 @echo off
-REM Keszitette: Tim Andras @ 2008
+REM Keszitette: Tim Andras @ 2009
 
 title RENDSZER KARBANTARTµSA   [Ind¡t s...]
 
@@ -11,59 +11,12 @@ if defined dirPRG goto Inditoval
 :Inditoval
 call "%dirPRG%\scripts\#init\set_path.cmd" %1
 
-rem =======================================================================================================================================================================================
-rem =======================================================================================================================================================================================
-
 rem ---KIERTEKELES-------------------------------------------------------------------------------------------------------------------
-set temporary=_
-set defrag=_
-set virscan=V
-set poweroff=_
-set usn=_
-
-if /i "%2" == "-ALL" (
-  set temporary=T
-  set defrag=D
-  set usn=P
-  goto LetsStart
-)
-
-if /i "%2" == "-Temporary" set temporary=T
-if /i "%3" == "-Temporary" set temporary=T
-if /i "%4" == "-Temporary" set temporary=T
-if /i "%5" == "-Temporary" set temporary=T
-if /i "%6" == "-Temporary" set temporary=T
-
-if /i "%2" == "-USN" set usn=U
-if /i "%3" == "-USN" set usn=U
-if /i "%4" == "-USN" set usn=U
-if /i "%5" == "-USN" set usn=U
-if /i "%6" == "-USN" set usn=U
-
-if /i "%2" == "-Defrag" set defrag=D
-if /i "%3" == "-Defrag" set defrag=D
-if /i "%4" == "-Defrag" set defrag=D
-if /i "%5" == "-Defrag" set defrag=D
-if /i "%6" == "-Defrag" set defrag=D
-
-if /i "%2" == "-NoVScan" set virscan=_
-if /i "%3" == "-NoVScan" set virscan=_
-if /i "%4" == "-NoVScan" set virscan=_
-if /i "%5" == "-NoVScan" set virscan=_
-if /i "%6" == "-NoVScan" set virscan=_
-
-if /i "%2" == "-PowerOff" set poweroff=P
-if /i "%3" == "-PowerOff" set poweroff=P
-if /i "%4" == "-PowerOff" set poweroff=P
-if /i "%5" == "-PowerOff" set poweroff=P
-if /i "%6" == "-PowerOff" set poweroff=P
-
-
-:LetsStart
+call "%dirPRG%\scripts\#init\set_params.cmd" %*
 title RENDSZER KARBANTARTµSA   [%temporary%%usn%%defrag%%virscan%%poweroff%]
 
-
-
+rem =======================================================================================================================================================================================
+rem =======================================================================================================================================================================================
 
 rem ---CLEAR---------------------------------------------------------------------------------------------------------------------------------
 call %sScript% clear reg-recent.cmd
---
+++ scripts/#init/set_params.cmd
@@ -0,0 +1,28 @@
+@echo off
+REM Keszitette: Tim Andras @ 2009
+
+set ALL=_
+rem -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+set temporary=_
+set usn=_
+set defrag=_
+set virscan=_
+set poweroff=_
+rem -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+set i=1
+:ciKiertekeles
+	if /i "%1" == "-ALL" set ALL=A
+	if /i "%1" == "-Temporary" set temporary=T
+	if /i "%1" == "-USN" set usn=U
+	if /i "%1" == "-Defrag" set defrag=D
+	if /i "%1" == "-VScan" set virscan=V
+	if /i "%1" == "-PowerOff" set poweroff=P
+shift && if %i% LSS 9 set /A i=i+1 && goto ciKiertekeles
+
+if "%ALL%" == "A" (
+	set temporary=T
+	set usn=U
+	set defrag=D
+	set virscan=V
+)
