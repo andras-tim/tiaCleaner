ec67ab2d7b28a95e103f12bfff7a5fd24d5f72ea
Andras Tim <andras.tim@gmail.com>
2009-01-23 02:49:38 +0100

    Temporary: IE 5,6,7 támogatás
        most + Google Chrome

--- scripts/clear/temporary.cmd
+++ scripts/clear/temporary.cmd
@@ -1,6 +1,9 @@
 @echo off
-REM Keszitette: Tim Andras @ 2008
+REM Keszitette: Tim Andras @ 2009
 
 echo  * Internetes gyors°t¢t†rak kiÅr°tÇse...
 set mappa=%USERPROFILE%\Local Settings\Temporary Internet Files\Content.IE5
+call "%aMAPP%\#erase-d.cmd"
+
+set mappa=%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data\Default\Cache
 call "%aMAPP%\#erase-d.cmd"
