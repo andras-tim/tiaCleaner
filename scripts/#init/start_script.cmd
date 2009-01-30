b005e7ded4198a2637bf9c4bb51bb71c5949ddfb
Andras Tim <andras.tim@gmail.com>
2009-01-30 02:32:39 +0100

    [start_script.cmd] paramétereket is átad most már

--- scripts/#init/start_script.cmd
+++ scripts/#init/start_script.cmd
@@ -1,11 +1,14 @@
 @echo off
-REM Keszitette: Tim Andras @ 2008
+REM Keszitette: Tim Andras @ 2010
 
 if exist "%null1%" del "%null1%"
 if exist "%null2%" del "%null2%"
 
 set aMAPP=%dirSCRIPTS%\%1
-call "%aMAPP%\%2"
+shift
+set aSCRIPT=%1
+shift
+call "%aMAPP%\%aSCRIPT%" %1 %2 %3 %4 %5 %6 %7
 
 if exist "%null1%" del "%null1%"
 if exist "%null2%" del "%null2%"
