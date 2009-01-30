ba5befeb4e3f0dbca6a037a5949239143f77d6ab
Andras Tim <andras.tim@gmail.com>
2009-01-30 01:46:17 +0100

    új starter.cmd      o ki lett bővítve több profillal      o van súgó      o a virscan átalakítások le lettek követve

--- maxi_karbantartas.cmd
+++ maxi_karbantartas.cmd
@@ -1,5 +1,5 @@
 @echo off
-REM Keszitette: Tim Andras @ 2009
+REM Keszitette: Tim Andras @ 2010
 
 title RENDSZER KARBANTARTµSA   [Ind¡t s...]
 
@@ -13,17 +13,20 @@ call "%dirPRG%\scripts\#init\set_path.cmd" %1
 
 rem ---KIERTEKELES-------------------------------------------------------------------------------------------------------------------
 call "%dirPRG%\scripts\#init\set_params.cmd" %*
-title RENDSZER KARBANTARTµSA   [%temporary%%logs%%usn%%defrag%%virscan%%poweroff%]
+title RENDSZER KARBANTARTµSA   [%memvirscan%%temporary%%recent%%logs%%usn%%defrag%%virscan%%poweroff%]
 
 rem =======================================================================================================================================================================================
 rem =======================================================================================================================================================================================
+
+rem ---MEM VIRSCAN---------------------------------------------------------------------------------------------------------------------------
+if "%memvirscan%" == "M" call %sScript% virscan starter.cmd -mem"
 
 rem ---CLEAR---------------------------------------------------------------------------------------------------------------------------------
-call %sScript% clear reg-recent.cmd
-rem call %sScript% clear recent.cmd
 call %sScript% clear temp.cmd
 call %sScript% clear install.cmd
 if "%temporary%" == "T" call %sScript% clear temporary.cmd
+if "%recent%" == "R" call %sScript% clear reg-recent.cmd
+if "%recent%" == "R" call %sScript% clear recent.cmd
 if "%logs%" == "L" call %sScript% clear logs.cmd
 if "%usn%" == "U" call %sScript% clear fs.cmd
 
@@ -31,7 +34,7 @@ rem ---DEFRAG-------------------------------------------------------------------
 if "%defrag%" == "D" call %sScript% defrag all.cmd"
 
 rem ---VIRSCAN-------------------------------------------------------------------------------------------------------------------------------
-if "%virscan%" == "V" call %sScript% virscan all.cmd"
+if "%virscan%" == "V" call %sScript% virscan starter.cmd -hdd"
 
 rem ---POWER OFF---------------------------------------------------------------------------------------------------------------------------
 if "%poweroff%" == "P" shutdown -s -t 60 -c "A KARBANTARTµS BEFEJEZŠD™TT!   (A be ll¡t sok alapj n a g‚p ki fog kapcsolni. Ha ezt meg k¡v nja szak¡tani, futtassa a 'SHUTDOWN -A' parancsot)"
--- scripts/#init/set_params.cmd
+++ scripts/#init/set_params.cmd
@@ -1,20 +1,33 @@
 @echo off
-REM Keszitette: Tim Andras @ 2009
+REM Keszitette: Tim Andras @ 2010
 
+set BASE=_
+set NORMAL=_
 set ALL=_
 rem -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 set temporary=_
+set recent=_
 set logs=_
 set usn=_
 set defrag=_
+set memvirscan=_
 set virscan=_
 set poweroff=_
 rem -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 set i=1
 :ciKiertekeles
+	if /i "%1" == "/?" goto sugo
+	if /i "%1" == "-?" goto sugo
+	if /i "%1" == "--help" goto sugo
+
+	if /i "%1" == "-BASE" set BASE=B
+	if /i "%1" == "-NORMAL" set NORMAL=N
 	if /i "%1" == "-ALL" set ALL=A
+
+	if /i "%1" == "-MVScan" set memvirscan=M
 	if /i "%1" == "-Temporary" set temporary=T
+	if /i "%1" == "-Recent" set recent=R
 	if /i "%1" == "-Log" set logs=L
 	if /i "%1" == "-USN" set usn=U
 	if /i "%1" == "-Defrag" set defrag=D
@@ -22,10 +35,63 @@ set i=1
 	if /i "%1" == "-PowerOff" set poweroff=P
 shift && if %i% LSS 9 set /A i=i+1 && goto ciKiertekeles
 
-if "%ALL%" == "A" (
+if "%BASE%" == "B" (
+	set memvirscan=M
 	set temporary=T
+	rem set recent=R
+	set logs=L
+	rem set usn=U
+	rem set defrag=D
+	rem set virscan=V
+	rem set poweroff=P
+)
+
+if "%NORMAL%" == "N" (
+	set memvirscan=M
+	set temporary=T
+	rem set recent=R
+	set logs=L
+	set usn=U
+	set defrag=D
+	rem set virscan=V
+	rem set poweroff=P
+)
+
+if "%ALL%" == "A" (
+	set memvirscan=M
+	set temporary=T
+	set recent=R
 	set logs=L
 	set usn=U
 	set defrag=D
 	set virscan=V
+	rem set poweroff=P
 )
+
+if "%virscan%" == "V" (
+    set memvirscan=M
+)
+
+goto tovabb
+
+:sugo
+echo TiA Karbantart†s - Tim Andras @ 2010
+echo.
+echo ParamÇterek:
+echo   -Temporary   BîngÇszãk gyors°t¢t†rainak tîrtlÇse
+echo   -Recent      ElãzmÇnyek tîrlÇse
+echo   -Log         Napl¢f†jlok tîrtlÇse
+echo   -USN         USN napl¢ År°tÇse
+echo   -Defrag      F†jlrendszer tîredeztettsÇg mentes°tÇse
+echo   -MVScan      Mem¢ria v°rusellenãrzÇse
+echo   -VScan       Meme¢ria Çs a f†jlrendszer v°rusellenãrzÇse
+echo   -PowerOff    A feladatok vÇgÇn a sz†m°t¢gÇp kikapcsol†sa
+echo.
+echo Profilok:
+echo   -BASE        MVScan, Temporary, Log
+echo   -NORMAL      MVScan, Temporary, Log, USN, Defrag
+echo   -ALL         Az îsszes paramÇter kivÇve a PowerOff
+echo.
+exit
+
+:tovabb
