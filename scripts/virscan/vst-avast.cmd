81c3476a514470767018c7aedffc24df0c400995
Andras Tim <andras.tim@gmail.com>
2009-02-06 22:03:22 +0100

    [virscan] acast tool-nal ne induljon el mem ellenorzeskor a fajl ellenorzes

--- scripts/virscan/vst-avast.cmd
+++ scripts/virscan/vst-avast.cmd
@@ -3,9 +3,9 @@ REM Keszitette: Tim Andras @ 2010
 
 if /i "%1" == "-hdd" (
     echo          * Merevlemezek, registry ellen‹rz‚se...
-    "%dirTMP%\tmp_virscan_tool.exe" /M /R /L
+    "%dirTMP%\tmp_virscan_tool.exe" /R /L /M
 )
 if /i "%1" == "-mem" (
     echo          * Mem¢ria ellen‹rz‚se...
-    "%dirTMP%\tmp_virscan_tool.exe" /T /R /L
+    "%dirTMP%\tmp_virscan_tool.exe" /R /L /T=\__VEGE:__/
 )
