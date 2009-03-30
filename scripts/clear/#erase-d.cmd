8fdfee2d5cb77fc1628af99fb7dbd756ba1114d5
Andras Tim <andras.tim@gmail.com>
2009-03-31 01:46:14 +0200

    [clean] erase-d mappa törlés felokosítása
     * mostmár nem az egész mappát törli, hanem csak a tartalmát

--- scripts/clear/#erase-d.cmd
+++ scripts/clear/#erase-d.cmd
@@ -1,22 +1,32 @@
 @echo off
-REM Keszitette: Tim Andras @ 2008
+REM Keszitette: Tim Andras @ 2010
 
 if "%mappa%" == "" exit
-if not exist "%mappa%\desktop.ini" goto CsakTorol
+set voltakFajlok=false
+if exist "%mappa%\desktop.ini" goto backup
+goto clean
 
-
+:backup
+set voltakFajlok=true
 attrib "%mappa%\desktop.ini" -s -h > "%null1%" 2> "%null2%"
-move "%mappa%\desktop.ini" "%mappa%_desktop.ini" > "%null1%" 2> "%null2%"
+move "%mappa%\desktop.ini" "%mappa%_tmp_desktop.ini" > "%null1%" 2> "%null2%"
 
-rmdir /s /q "%mappa%" > "%null1%" 2> "%null2%"
-mkdir "%mappa%" > "%null1%" 2> "%null2%"
+:clean
+for /R "%mappa%" /D %%m in (*) do (
+    rmdir /s /q "%%m" > "%null1%" 2> "%null2%"
+    for %%f in ("%%m\*") do (
+        del /f /s /a /q "%%f" > "%null1%" 2> "%null2%"
+    )
+)
+for %%f in ("%mappa%\*") do (
+    del /f /s /a /q "%%f" > "%null1%" 2> "%null2%"
+)
 
-move "%mappa%_desktop.ini" "%mappa%\desktop.ini" > "%null1%" 2> "%null2%"
-attrib "%mappa%\desktop.ini" +s +h > "%null1%" 2> "%null2%"
+if "%voltakFajlok%" == "true" goto restore
 goto vege
 
-:CsakTorol
-rmdir /s /q "%mappa%" > "%null1%" 2> "%null2%"
-mkdir "%mappa%" > "%null1%" 2> "%null2%"
+:restore
+move "%mappa%_tmp_desktop.ini" "%mappa%\desktop.ini" > "%null1%" 2> "%null2%"
+attrib "%mappa%\desktop.ini" +s +h > "%null1%" 2> "%null2%"
 
 :vege
