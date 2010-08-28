c879703f5ecffbaf4c0efaa403bc7d8dac9c6a99
Andras Tim <andras.tim@gmail.com>
2010-08-28 14:44:36 +0200

    [clear] Fixed titles

--- scripts/clear/temp.cmd
+++ scripts/clear/temp.cmd
@@ -21,14 +21,17 @@ set mappa=%ALLUSERSPROFILE%\..\NetworkService\Local Settings\Temp
 call "%aMAPP%\#erase-d.cmd"
 
 echo      * Programspecifikus...
+echo          * Microsoft Office...
 rmdir /s /q "%USERPROFILE%\Local Settings\Temporary Internet Files\Content.MSO" > "%null1%" 2> "%null2%"
 rmdir /s /q "%USERPROFILE%\Local Settings\Temporary Internet Files\Content.Word" > "%null1%" 2> "%null2%"
 rmdir /s /q "%USERPROFILE%\Local Settings\Temporary Internet Files\Content.Excel" > "%null1%" 2> "%null2%"
 rmdir /s /q "%USERPROFILE%\Local Settings\Temporary Internet Files\OLK???" > "%null1%" 2> "%null2%"
 
+rem echo          * Google Earth...
 rem del "%USERPROFILE%\Local Settings\Application Data\Google\GoogleEarth\dbCache.dat" > "%null1%" 2> "%null2%"
 rem del "%USERPROFILE%\Local Settings\Application Data\Google\GoogleEarth\dbCache.dat.index" > "%null1%" 2> "%null2%"
 
+echo          * Egy‚b...
 rem MSOCache, CloneDVD
 start "" /wait "%aMAPP%\temp_seged.vbs"
 call "%dirTMP%\tmp_clear_temp.cmd"
--- scripts/clear/temporary.cmd
+++ scripts/clear/temporary.cmd
@@ -2,6 +2,7 @@
 REM Keszitette: Tim Andras @ 2010
 
 echo  * Internetes gyors°t¢t†rak kiÅr°tÇse...
+echo      * Internet Explorer...
 set mappa=%ALLUSERSPROFILE%\..\LocalService\Local Settings\Temporary Internet Files\Content.IE5
 call "%aMAPP%\#erase-d.cmd"
 
@@ -11,6 +12,6 @@ call "%aMAPP%\#erase-d.cmd"
 set mappa=%USERPROFILE%\Local Settings\Temporary Internet Files\Content.IE5
 call "%aMAPP%\#erase-d.cmd"
 
-
+echo      * Google Chrome...
 set mappa=%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data\Default\Cache
 call "%aMAPP%\#erase-d.cmd"
